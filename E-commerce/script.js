3eb2f2d5b4b6c746022251555af6e31b22ac4b95
Buhle5969 <cimeleesethu@gmail.com>
2024-07-27 14:37:42 +0200

    Add files via upload

---
+++ E-commerce/script.js
@@ -0,0 +1,23 @@
+function addToCart() {
+alert("Item added to cart!");
+}
+
+function removeFromCart() {
+ alert("Item removed from cart!");   
+}
+
+function checkout() {
+alert("Proceeding to checkout!"); 
+}
+
+function submitOrder() {
+ alert("Order submitted!")  
+}
+
+function login() {
+alert("Logging in..."); 
+}
+
+function register() {
+alert ("Registering...");
+}
